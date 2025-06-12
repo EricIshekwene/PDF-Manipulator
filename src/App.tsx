e69d9a554fb42fccef18915376068adcfbc84267
Eric Ishekwene <ericishcollege@gmail.com>
2025-06-12 19:11:26 +0200

    fix: update dependencies and fix deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "./navBar";
 import PdfOptions from "./pdfOptions";
 import FileAdder from "./fileAdder";
--- src/pdfOptions.tsx
+++ src/pdfOptions.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface PdfOptionsProps {
     selectedOption: string | null;
     onOptionChange: (option: string) => void;
