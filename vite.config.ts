bb0b46d8896246e218521ca49b86a76aaa3936b8
Eric Ishekwene <ericishcollege@gmail.com>
2025-06-13 18:24:05 +0200

    Update logic / UI / styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-  base: process.env.NODE_ENV === 'production' ? '/PDF-Manipulator/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  server: {
-    port: 3000,
-  }
+  base: "/PDF-Manipulator/", 
+  plugins: [react(), tsconfigPaths()],
 });
